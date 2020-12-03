4a36ca7c3f3ae6c398eac42a2f06afa41e504d75
Evan Smith <esincork@gmail.com>
2020-12-04 00:26:53 +0100

    Add a simple test

--- tests/test_daft-scraper.py
+++ tests/test_daft-scraper.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import json
 import unittest
+
+from daft_scraper import Daft
 
 
 class TestDaftScraper(unittest.TestCase):
@@ -9,8 +12,11 @@ class TestDaftScraper(unittest.TestCase):
     def setUp(self):
         pass
 
-    def test_something(self):
-        pass
-
     def tearDown(self):
         pass
+
+    def test_confirm_next_json_parseable(self):
+        api = Daft()
+        webpage = api.get('/property-for-rent/ireland')
+        next_script_text = webpage.find('script', {'id': '__NEXT_DATA__'})
+        json.loads(next_script_text.string)
