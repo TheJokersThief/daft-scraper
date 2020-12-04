db07355675c8b47d25dd66f7e64aa33b193af473
Evan Smith <esincork@gmail.com>
2020-12-04 18:21:05 +0100

    DAFT-3: Add search (#3)

---
+++ daft_scraper/search/options.py
@@ -0,0 +1,188 @@
+import sys
+from dataclasses import dataclass
+from enum import Enum
+from typing import List
+
+
+class Option():
+    def get_params(self):
+        return {}
+
+
+@dataclass
+class PriceOption(Option):
+    min: int = 0
+    max: int = sys.maxsize
+
+    PARAM_KEY = "rentalPrice"
+
+    def get_params(self):
+        return {
+            f"{PriceOption.PARAM_KEY}_from": self.min,
+            f"{PriceOption.PARAM_KEY}_to": self.max,
+        }
+
+
+@dataclass
+class BedOption(Option):
+    min: int = 0
+    max: int = sys.maxsize
+
+    PARAM_KEY = "numBeds"
+
+    def get_params(self):
+        return {
+            f"{BedOption.PARAM_KEY}_from": self.min,
+            f"{BedOption.PARAM_KEY}_to": self.max,
+        }
+
+
+@dataclass
+class LeaseLengthOption(Option):
+    min: int = 0
+    max: int = sys.maxsize
+
+    PARAM_KEY = "leaseLength"
+
+    def get_params(self):
+        return {
+            f"{LeaseLengthOption.PARAM_KEY}_from": self.min,
+            f"{LeaseLengthOption.PARAM_KEY}_to": self.max,
+        }
+
+
+class PropertyType(Enum):
+    ALL = None
+    HOUSE = "houses"
+    APARTMENT = "apartments"
+    STUDIO_APARTMENT = "studio-apartments"
+
+
+@dataclass
+class PropertyTypesOption(Option):
+    property_types: List[PropertyType]
+
+    def get_params(self):
+        if PropertyType.ALL in self.property_types:
+            # If we want "all", don't submit any params
+            return {}
+
+        return {
+            "propertyType": [propType.value for propType in self.property_types]
+        }
+
+
+class Facility(Enum):
+    NONE = None
+    ALARM = "alarm"
+    CABLE_TELEVISION = "cable-television"
+    DISHWASHER = "dishwasher"
+    GARDEN_PATIO_BALCONY = "garden-patio-balcony"
+    CENTRAL_HEATING = "central-heating"
+    INTERNET = "internet"
+    MICROWAVE = "microwave"
+    PARKING = "parking"
+    PETS_ALLOWED = "pets-allowed"
+    SMOKING = "smoking"
+    SERVICED_PROPERTY = "serviced-property"
+    DRYER = "dryer"
+    WASHING_MACHINE = "washing-machine"
+    WHEELCHAIR_ACCESS = "wheelchair-access"
+
+
+@dataclass
+class FacilitiesOption(Option):
+    facilities: List[Facility]
+
+    def get_params(self):
+        if self.facilities == [Facility.NONE]:
+            # If facility list specifies none, don't return params
+            return {}
+
+        return {
+            "facilities": [facil.value for facil in self.facilities]
+        }
+
+
+class MediaType(Enum):
+    ALL = None
+    VIDEO = "video"
+    VIRTUAL_TOUR = "virtual-tour"
+
+
+@dataclass
+class MediaTypesOption(Option):
+    media_types: List[MediaType]
+
+    def get_params(self):
+        if MediaType.ALL in self.media_types:
+            # If we want "all", don't submit any params
+            return {}
+
+        return {
+            "mediaTypes": [propType.value for propType in self.media_types]
+        }
+
+
+@dataclass
+class KeywordsOption(Option):
+    terms = List[str]
+
+    def get_params(self):
+        return {
+            "terms": self.terms
+        }
+
+
+class AdState(Enum):
+    AVAILABLE = "published"
+    AGREED = "sale-agreed"
+
+
+@dataclass
+class AdStateOption(Option):
+    ad_state: AdState
+
+    def get_params(self):
+        return {
+            "adState": self.ad_state.value
+        }
+
+
+class Sort(Enum):
+    BEST_MATCH = None
+    MOST_RECENT = "publishDateDesc"
+    PRICE_ASCENDING = "priceAsc"
+    PRICE_DESCENDING = "priceDesc"
+
+
+@dataclass
+class SortOption(Option):
+    sort: Sort
+
+    def get_params(self):
+        if self.sort == Sort.BEST_MATCH:
+            return {}
+
+        return {
+            "sort": self.sort.value
+        }
+
+
+class Furnishing(Enum):
+    ALL_FURNISHINGS = None
+    FURNISHED = "furnished"
+    UNFURNISHED = "unfurnished"
+
+
+@dataclass
+class FurnishingOption(Option):
+    furnishing: Furnishing
+
+    def get_params(self):
+        if self.furnishing == Furnishing.ALL_FURNISHINGS:
+            return {}
+
+        return {
+            "furnishing": self.furnishing.value
+        }
---
+++ tests/test_search.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import mock
+import unittest
+
+from daft_scraper.search import DaftSearch, SearchType
+from daft_scraper.search.options import PropertyType, PropertyTypesOption, Facility, FacilitiesOption, PriceOption, BedOption
+from daft_scraper.search.options_location import LocationsOption, Location
+
+
+with open('tests/fixtures/sample_page.html') as file:
+    SEARCH_PAGE_HTML = file.read()
+
+
+class TestDaftScraper(unittest.TestCase):
+
+    def setUp(self):
+        self.api = DaftSearch(SearchType.RENT)
+
+    def tearDown(self):
+        pass
+
+    @mock.patch('daft_scraper.Daft.get', return_value=SEARCH_PAGE_HTML)
+    def test_search(self, *args):
+        options = [
+            PropertyTypesOption([PropertyType.APARTMENT]),
+            FacilitiesOption([Facility.PARKING, Facility.SERVICED_PROPERTY]),
+            LocationsOption([Location.SWORDS_DUBLIN]),
+            PriceOption(0, 1000),
+            BedOption(1, 4),
+        ]
+
+        got = self.api.search(options)
+        self.assertEqual(got[0]['id'], 1443907)
+
+    def test__translate_options(self):
+        wanted = {
+            'propertyType': ['apartments'],
+            'facilities': ['parking', 'serviced-property'],
+            'Location': ['swords-dublin'],
+            'rentalPrice_from': 0,
+            'rentalPrice_to': 1000,
+            'numBeds_from': 1,
+            'numBeds_to': 4
+        }
+        options = [
+            PropertyTypesOption([PropertyType.APARTMENT]),
+            FacilitiesOption([Facility.PARKING, Facility.SERVICED_PROPERTY]),
+            LocationsOption([Location.SWORDS_DUBLIN]),
+            PriceOption(0, 1000),
+            BedOption(1, 4),
+        ]
+        got = self.api._translate_options(options)
+        self.assertEqual(got, wanted)
+
+    def test__build_search_path(self):
+        got = self.api._build_search_path()
+        self.assertEqual(got, "property-for-rent/ireland")
+
+    @mock.patch('daft_scraper.Daft.get', return_value=SEARCH_PAGE_HTML)
+    def test__get_page_data(self, *args):
+        path = self.api._build_search_path()
+        got = self.api._get_page_data(path, params={})
+        self.assertEqual(len(got.keys()), 12)
+
+    @mock.patch('daft_scraper.Daft.get', return_value=SEARCH_PAGE_HTML)
+    def test__get_listings(self, *args):
+        path = self.api._build_search_path()
+        page_data = self.api._get_page_data(path, params={})
+
+        got = self.api._get_listings(page_data['props']['pageProps']['listings'])
+        self.assertEqual(got[0]['id'], 1443907)
+
+    def test__calc_offset(self):
+        got = self.api._calc_offset(5)
+        self.assertEqual(got, 100)
