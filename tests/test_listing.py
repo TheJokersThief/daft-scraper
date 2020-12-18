1a450498f1288d0d496e24edab2cd1e77bbec9ef
Evan Smith <esincork@gmail.com>
2020-12-18 08:48:13 +0100

    Add new tests for ad page info

--- tests/test_listing.py
+++ tests/test_listing.py
@@ -2,9 +2,15 @@
 # -*- coding: utf-8 -*-
 
 import json
+import mock
 import unittest
+from bs4 import BeautifulSoup
 
-from daft_scraper.listing import Listing
+from daft_scraper.listing import ListingSchema, Listing
+
+
+with open('tests/fixtures/ad_page_info.html') as file:
+    AD_PAGE_HTML = BeautifulSoup(file.read(), features="html.parser")
 
 
 class TestDaftScraper(unittest.TestCase):
@@ -17,7 +23,7 @@ class TestDaftScraper(unittest.TestCase):
 
     def test_confirm_next_json_parseable(self):
         with open('tests/fixtures/listing.json', 'r') as fixture:
-            got = Listing().load(json.load(fixture)['listing'])
+            got = ListingSchema().load(json.load(fixture)['listing'])
 
         self.assertEqual(got.get('id'), 2315059)
         self.assertEqual(got.get('price'), 14443.52)
@@ -30,3 +36,13 @@ class TestDaftScraper(unittest.TestCase):
 
         # Test seller
         self.assertEqual(got.get('seller').get('sellerId'), 9601)
+
+    @mock.patch('daft_scraper.Daft.get', return_value=AD_PAGE_HTML)
+    def test_age_page_info(self, *args):
+        listing = None
+        with open('tests/fixtures/listing.json', 'r') as fixture:
+            listing = Listing(ListingSchema().load(json.load(fixture)['listing']))
+
+        self.assertEqual(listing.county, ["Cork"])
+        self.assertEqual(listing.area, ["cork-city"])
+        self.assertEqual(listing.views, 31956)
